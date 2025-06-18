ec876ce605bc9e059fd88700e7ee902a90bfa8f7
snehlatakvs29@gmail.com <snehlatakvs29@gamil.com>
2025-06-18 23:29:09 +0530

    Start new project Guess my number

---
+++ 05.Guess-My-Number/script.js
@@ -0,0 +1,27 @@
+//Guess my number --[game] -- with retro design --goal of this project is simply guess the secret number which is between 1 and 20
+'use strict';
+/*
+//                           ***Selecting and Manipulating Elements:***
+
+document.querySelector('.message');//this query selector is a method that available in the document object. And into the  method we need to pass a selector here as 'message'.
+console.log(document.querySelector('.message'));//output:<p class="message">Start guessing...</p>
+
+console.log(document.querySelector('.message').textContent);//.querySelector('.message')- this part select the elment from document and then .textContent- this part read the content of the selected elment from the document. o/p:Start guessing...
+
+//for setting the content for something else from it's original value on the web page:
+document.querySelector('.message').textContent = '🎉Correct number';
+//still the console value doesn't changed for that:🎉Correct number
+console.log(document.querySelector('.message').textContent);//o/p:
+
+//doing same with different element..........
+document.querySelector('.number').textContent = 19; 
+document.querySelector('.score').textContent = 13;
+
+//doing same with input field...
+console.log(document.querySelector('.guess').value);//o/p:blank in console as no user input value yet neither we set any value.
+document.querySelector('.guess').value = 23;// set the input value as like we have written it our selves in web page.
+console.log(document.querySelector('.guess').value);//o/p:23.
+
+*/
+
+
